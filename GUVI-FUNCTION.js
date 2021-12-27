1a1ee701c52de25f2af855f026d9c759869fec57
SUDIP <sidsip2015@gmail.com>
2021-12-27 22:34:48 +0530

    first commit

---
+++ GUVI-FUNCTION.js
@@ -0,0 +1,37 @@
+
+//functions
+//types of functions
+//few programs based on functions
+
+//functions:code reusability
+//categories
+//normal,anonymous,iife,and ES6 Arrow functions
+
+//functions are defined by the keyword function 
+//sytnax:function functionname(parameters){
+  
+  //body of the functions
+  
+    
+//}
+//for every function we need a return keyword
+
+//we have to define(writing a function) the function before it has to be called
+
+//if return keyword is missing then the output is undefined 
+
+// //for multplications
+// function multiply(a,b){
+//     return a*b;
+    
+// }
+// console.log(multiply(12,13));
+
+//passing an array to a function
+
+//for a given array write a js function to print only even numbers in the array
+//let arr=[1,2,3,4];
+//let res=[2,4];
+//2,4 
+
+//arrayname is nothing but the address of the array
